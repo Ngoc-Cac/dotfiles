c3d74591802667ec74bf25bbb7355f77246d10f0
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-01-06 09:46:53 +0700

    added more alias and updated comments

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-# for making symlinks
+# for making symlinks ON WINDOWS
 alias mklink="cmd //c mklink"
+alias vim="nvim"
 
 alias ll="ls -l"
 alias la="ls -Al"
 alias lh="ls -ld .?*"
+
+# verbose by default cause I'm anxious
+alias rm="rm -v"
+alias rv="rm"
+
+# list declared aliases and functions
+alias aliases="alias | sed 's/=.*//'"
+alias functions="declare -f | grep '^[a-z].* ()' | sed 's/{$//'"
 
 # quick git commands
 alias gs="git status"
