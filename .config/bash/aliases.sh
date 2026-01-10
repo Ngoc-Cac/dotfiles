d016f47bd047804eb378a68fd797e47615d73b96
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-01-10 12:29:24 +0700

    refactored code in aliases.sh

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 # for making symlinks ON WINDOWS
-alias mklink="cmd //c mklink"
-alias vim="nvim"
+[[ "$OSTYPE" =~ ^(msys|cygwin) ]] && alias mklink="cmd //c mklink"
 
-### File Listing ###
+### Listing commands ###
 # use eza instead of ls if available
-if command -v eza > /dev/null; then
-    alias ls="eza --icons=always --group-directories-first -h"
-fi
+command -v eza > /dev/null && alias ls="eza --icons=auto --group-directories-first -h"
+
 alias ll="ls -l"
 alias la="ls -Al"
 alias lh="ls -ld .?*"
 
-# verbose by default cause I'm anxious
-alias rm="rm -v"
-alias rv="rm"
-
 # list declared aliases and functions
 alias aliases="alias | sed 's/=.*//'"
 alias functions="declare -f | grep '^[a-z].* ()' | sed 's/{$//'"
+
+# verbose by default cause I'm anxious
+alias rm="rm -v"
+alias rv="rm"
 
 ### Git Commands ###
 alias gs="git status"
 alias gl="git log --graph --oneline"
 alias gla="gl --all"
 
+### Misc ###
+alias nv="nvim"
+alias vi="nvim"
+alias vim="nvim"
 alias conda_act="source ~/miniconda3/Scripts/activate"
 
 alias ..="cd .."
