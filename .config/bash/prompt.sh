c79d406c99ae6afd65fc2fe4b2052cc13cbaedb4
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-01-10 09:37:49 +0700

    added custom ps1 prompt

---
+++ .config/bash/prompt.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+_exit_code_info() { [[ $? -eq 0 ]] && printf "\033[32m" || printf "\033[31m󰬅"; }
+_git_info() {
+    local exit_code=$?
+
+    local upstream=$(git rev-parse --abbrev-ref @{upstream} 2>/dev/null)
+    [[ -z "$upstream" ]] && return $exit_code
+
+    local ahead=$(git rev-list --count ${upstream}..HEAD 2>/dev/null)
+    local behind=$(git rev-list --count HEAD..${upstream} 2>/dev/null)
+
+    local counts=""
+    [[ "$behind" -gt 0 ]] && counts="\033[91m↓$behind "
+    [[ "$ahead" -gt 0 ]] && counts="$counts\033[92m↑$ahead"
+
+    local branch=$(__git_ps1 "\033[35m%s") 
+    printf "($branch $counts)"
+
+    return $exit_code
+}
+_conda_info() {
+    local exit_code=$?
+
+    [[ -n "$CONDA_DEFAULT_ENV" ]] && printf "(%s) " "$CONDA_DEFAULT_ENV"
+
+    return $exit_code
+}
+
+
+export CONDA_CHANGEPS1=false
+
+PS1='$(_conda_info)\033[34m\u@\h \033[93m󰉋 \w \033[0m$(_git_info)'
+PS1+='\012$(_exit_code_info) ~#@❯\033[0m  '
