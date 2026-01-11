ec1c576f58c827e1e98a36344c974f89a1af28d3
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-01-11 08:20:54 +0700

    added more aliases

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -2,7 +2,7 @@
 # for making symlinks ON WINDOWS
 [[ "$OSTYPE" =~ ^(msys|cygwin) ]] && alias mklink="cmd //c mklink"
 
-### Listing commands ###
+### Listing Commands ###
 # use eza instead of ls if available
 command -v eza > /dev/null && alias ls="eza --icons=auto --group-directories-first -h"
 
@@ -14,6 +14,11 @@ alias lh="ls -ld .?*"
 alias aliases="alias | sed 's/=.*//'"
 alias functions="declare -f | grep '^[a-z].* ()' | sed 's/{$//'"
 
+### File Modification ###
+# i to confirm before overwriting
+alias mv="mv -vi"
+alias cp="cp -vi"
+
 # verbose by default cause I'm anxious
 alias rm="rm -v"
 alias rv="rm"
@@ -24,6 +29,7 @@ alias gl="git log --graph --oneline"
 alias gla="gl --all"
 
 ### Misc ###
+alias cl="clear"
 alias nv="nvim"
 alias vi="nvim"
 alias vim="nvim"
