71d1889fac2b739523e9f2ef6eef1f86e962d987
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-01-17 08:30:38 +0700

    updated windows aliases

--- .config/bash/aliases_win.sh
+++ .config/bash/aliases_win.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
 # for making symlinks ON WINDOWS
 alias mklink='cmd //c mklink'
 alias sudo='gsudo'
+
+# windows explorer
+# see this https://github.com/microsoft/WSL/issues/6565 for why the function 
+explorer() { command explorer "$@" || true; }
+alias wx="explorer"
