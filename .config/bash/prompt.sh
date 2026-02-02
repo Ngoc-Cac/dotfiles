b5db90e9da2091b80d3eb7065f8b01bfd0b668ca
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-02-02 07:12:22 +0700

    Refactor PS1 prompt
    Remove the whole git info thingy because it was way to slow.
    
    Falling back to the __git_ps1 now because it's much faster. Of course it
    means no fancy glyphs and all that.

--- .config/bash/prompt.sh
+++ .config/bash/prompt.sh
@@ -12,31 +12,15 @@ RESET="\033[0m"
 BOLD="\033[1m"
 
 _exit_code_info() { [[ $? -eq 0 ]] && printf "$GREEN" || printf "$RED"; }
-_git_info() {
-    local exit_code=$?
-
-    # check for upstream repo, 2> redirects ONLY stderr to /dev/null
-    local upstream=$(git rev-parse --abbrev-ref @{upstream} 2>/dev/null)
-    [[ -z "$upstream" ]] && return $exit_code
-
-    local ahead=$(git rev-list --count ${upstream}..HEAD 2>/dev/null)
-    local behind=$(git rev-list --count HEAD..${upstream} 2>/dev/null)
-
-    local counts=""
-    [[ "$behind" -gt 0 ]] && counts="$YELLOW↓$behind "
-    [[ "$ahead" -gt 0 ]] && counts+="$BGREEN↑$ahead"
-
-    printf "\033[0;1m$RESET $(__git_ps1 "$MAGENTA%s$BOLD") $counts$RESET"
-
-    return $exit_code
-}
-
 
 # if not set, conda will throw error because it can't
 # parse the ps1 command 
 export PYTHONIOENCODING=utf-8
+export GIT_PS1_SHOWUPSTREAM="verbose"
+export GIT_PS1_SHOWCOLORHINTS=true
 
-PS1="[$BLUE\u@\h$RESET] $BYELLOW󰉋 \w \$(_git_info)"
-PS1+="\012\$(_exit_code_info) ~#@❯$RESET  "
+PS1="[\[$BLUE\]\u@\h\[$RESET\]] \[$BYELLOW\]󰉋 \w "
+PS1+="\[$MAGENTA\]\$(__git_ps1 \"%s\")"
+PS1+=$'\n'"\$(_exit_code_info) ~#@❯\[$RESET\]  "
 
 PS2="  󱞪 "
