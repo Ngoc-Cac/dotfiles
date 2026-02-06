a82a615385381da92d13c3e4085e27d03c9d009c
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-02-06 19:24:05 +0700

    Update colour of PS1 prompt
    Change the directory to a darker yellow colour.
    
    Remove some unused colours.
    
    Also update the colour of the arrow

--- .config/bash/prompt.sh
+++ .config/bash/prompt.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
 BLUE="\033[34m"
-BGREEN="\033[92m"
 GREEN="\033[32m"
 MAGENTA="\033[35m"
 RED="\033[31m"
-BYELLOW="\033[93m"
 YELLOW="\033[33m"
+GRAY="\033[90m"
 
 RESET="\033[0m"
-BOLD="\033[1m"
 
 # \001 and \002 is equivalent to \[ \]
 _exit_code_info() { [[ $? -eq 0 ]] && printf "\001$GREEN\002" || printf "\001$RED\002"; }
@@ -24,8 +22,8 @@ export GIT_PS1_SHOWCONFLICTSTATE=yes
 
 PROMPT_COMMAND='_PS1_GIT=$(__git_ps1 "%s"); _EXIT_CODE_SYMBOL=$(_exit_code_info)'
 
-PS1="[\[$BLUE\]\u@\h\[$RESET\]] \[$BYELLOW\]󰉋 \w "
+PS1="[\[$BLUE\]\u@\h\[$RESET\]] \[$YELLOW\]󰉋 \w "
 PS1+="\[$MAGENTA\]\$_PS1_GIT"
-PS1+=$'\n'"\$_EXIT_CODE_SYMBOL ~#@❯\[$RESET\]  "
+PS1+=$'\n'"\$_EXIT_CODE_SYMBOL \[$GRAY\]~#@❯\[$RESET\]  "
 
 PS2="  󱞪 "
