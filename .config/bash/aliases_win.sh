27eba11e4a223bd2266dfcc2e5478d22ff9217f2
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-02-19 15:35:26 +0700

    Remove unused aliases on windows

--- .config/bash/aliases_win.sh
+++ .config/bash/aliases_win.sh
@@ -2,8 +2,3 @@
 # for making symlinks ON WINDOWS
 alias mklink='cmd //c mklink'
 alias sudo='gsudo'
-
-# windows explorer
-# see this https://github.com/microsoft/WSL/issues/6565 for why the function 
-explorer() { command explorer "$@" || true; }
-alias wx="explorer"
