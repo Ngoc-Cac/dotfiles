f50d1bcf631cd25ad805b85435e650cf886d1fc5
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-03-13 09:41:44 +0700

    Add guard clause for `enable_tip`
    The `enable_tip` configuration seems to be a new addition and isn't
    supported in Python 3.12.

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -5,8 +5,9 @@ eza =  "eza --icons=auto --group-directories-first --git -h"
 c = get_config()  #noqa
 
 c.TerminalInteractiveShell.editing_mode = 'vi'
-c.TerminalInteractiveShell.enable_tip = False
 c.TerminalInteractiveShell.term_title = False
+if hasattr(c.TerminalInteractiveShellm, "enable_tip"):
+    c.TerminalInteractiveShell.enable_tip = False
 
 c.AliasManager.user_aliases = [
     ("clear", "clear"),
