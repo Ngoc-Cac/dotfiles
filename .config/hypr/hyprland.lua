c324ad940169b39bbb04bf66587fb8786fc73e33
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-06-02 20:56:11 +0700

    Update startup applications
    Remove the `gsettings` because that does not need to be run every
    startup. ALso add startup command for some hyprland helpers

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -2,15 +2,21 @@ local CONFIG = require("config")
 
 
 local startup = {
-  "systemctl",  --user start hyprpolkitagent
-  "gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'",
-  "waybar",
-  CONFIG["terminal"],
+  "systemctl --user start hyprpolkitagent",
+
+  -- idle inhibitor and wallpaper
+  "hyprpaper", "hypridle",
+
+  -- wifi applet
+  "nm-applet --indicator", "blueman-applet",
+
+  "waybar", CONFIG["terminal"],
 }
 
 hl.on("hyprland.start", function()
   for _, command in ipairs(startup) do hl.exec_cmd(command) end
 end)
+
 
 hl.config({
   general = {
