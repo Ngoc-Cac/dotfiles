a36490a7274c89701e33ea09859ef0d14ee74869
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-06-12 19:22:24 +0700

    Refactor the config block to `config.lua`

--- .config/hypr/config.lua
+++ .config/hypr/config.lua
@@ -1,4 +1,40 @@
 return {
+  hypr_config = {
+    general = {
+      col = {
+        active_border   = { colors = {"rgb(ebdbb2)", "rgb(d65d0e)"}, angle = 30 },
+        inactive_border = "rgba(595959aa)",
+      },
+      no_focus_fallback = true,  -- no wrapping basically
+
+      resize_on_border = true,  -- resize by clicking and dargging
+      border_size = 2
+    },
+
+    decoration = {
+      rounding = 5,
+      rounding_power = 2,
+
+      active_opacity = 1.0,
+      inactive_opacity = 0.8,
+
+      blur = {
+        enabled = true,
+        size = 3,
+        passes = 3,
+        vibrancy = 0.1696
+      },
+    },
+
+    dwindle = {
+      force_split = 2,
+      preserve_split = true
+    },
+
+    misc = {
+      disable_hyprland_logo = true
+    }
+  },
   main_mod     = "SUPER",
   launcher     = "rofi -show drun",
   terminal     = "kitty",
--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -18,35 +18,7 @@ hl.on("hyprland.start", function()
 end)
 
 
-hl.config({
-  general = {
-    col = {
-      active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
-      inactive_border = "rgba(595959aa)",
-    },
-    no_focus_fallback = true,  -- no wrapping basically
-
-    resize_on_border = true,  -- resize by clicking and dargging
-    border_size = 2,
-  },
-
-  decoration = {
-    rounding = 5,
-    rounding_power = 2,
-
-    active_opacity = 1.0,
-    inactive_opacity = 0.8,
-
-    blur = {
-      enabled = true,
-      size = 3,
-      passes = 1,
-      vibrancy = 0.1696,
-    },
-  },
-
-  dwindle = { force_split = 2 }
-})
+hl.config(CONFIG["hypr_config"])
 
 
 -- hl.monitor({
