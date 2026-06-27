8f8791ed987f09526ecbb46cdcee34fd7b1181ec
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-06-27 20:23:03 +0700

    Update keybinds in HJKL mode
    Remove the redudant code because the bug was fixed.
    
    Add some other keybinds.

--- .config/hypr/modules/binds/hjkl_mode.lua
+++ .config/hypr/modules/binds/hjkl_mode.lua
@@ -4,12 +4,16 @@ hl.bind("code:66", hl.dsp.submap("HJKL Mode"))
 hl.define_submap("HJKL Mode", function()
   hl.bind("code:66", hl.dsp.submap("reset"))
 
-  -- weird bug: https://github.com/hyprwm/Hyprland/discussions/14445#discussioncomment-16894377
   for k, arrow in pairs(arrow_map) do
-    hl.bind(k, hl.dsp.send_shortcut({ mods = "", key = arrow }), { repeating = true })
-    hl.bind(k, hl.dsp.send_shortcut({ mods = "", key = arrow }), { release = true })
-
-    hl.bind("CTRL + " .. k, hl.dsp.send_shortcut({ mods = "CTRL", key = arrow }), { repeating = true })
-    hl.bind("CTRL + " .. k, hl.dsp.send_shortcut({ mods = "CTRL", key = arrow }), { release = true })
+    hl.bind(k, hl.dsp.send_shortcut({ mods = "", key = arrow }))
+    hl.bind("CTRL + " .. k, hl.dsp.send_shortcut({ mods = "CTRL", key = arrow }))
+    hl.bind("SHIFT + " .. k, hl.dsp.send_shortcut({ mods = "SHIFT", key = arrow }))
+    hl.bind(
+      "CTRL + SHIFT + " .. k,
+      hl.dsp.send_shortcut({ mods = "CTRL + SHIFT", key = arrow })
+    )
   end
+
+  hl.bind("CTRL + d", hl.dsp.send_shortcut({ mods = "", key = "mouse_down" }))
+  hl.bind("CTRL + u", hl.dsp.send_shortcut({ mods = "", key = "mouse_up" }))
 end)
