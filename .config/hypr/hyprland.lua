9ed0434e3e061fe5e3f9db91e7c41b7c35426b02
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-06-28 10:44:34 +0700

    Change auto-start behaviour of terminal
    Make the terminal start in a special workspace instead. There is no
    reason for it to occupy the whole default workspace anyway.

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -4,7 +4,6 @@ local CONFIG = require("config")
 local startup = {
   --- the commented lines should only be enabled if NOT using uwsm ---
   -- some syncing stuff for the xdg-desktop-portal-hyprland
-  -- "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP",
   -- "systemctl --user start hyprpolkitagent",
 
   -- wallpaper, idling and status bar
@@ -16,16 +15,15 @@ local startup = {
   -- input stuff
   "wl-paste --watch cliphist store",
   "fcitx5 -dr",
-
-  CONFIG["terminal"],
 }
 
 hl.on("hyprland.start", function()
   for _, command in ipairs(startup) do
     hl.exec_cmd("uwsm app -- " .. command)
   end
-end)
 
+  hl.dsp.exec_cmd(CONFIG["terminal"], { workspace = "special:term silent" })
+end)
 
 hl.config(CONFIG["hypr_config"])
 
