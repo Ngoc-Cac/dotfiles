c85fe4116cfacd715200ddf2891fe45d5493d002
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-06-28 10:41:05 +0700

    Refactor Rofi power menu utility
    Remove some unnecessary conditioning and branching. They are basically
    just repetition of previous code.
    
    Also move the font specification to the colorscheme specification.

--- .config/rofi/powermenu/powermenu.sh
+++ .config/rofi/powermenu/powermenu.sh
@@ -9,6 +9,17 @@ yes='Yes'
 no='No'
 
 
+session_logout() {
+    if [[ "$DESKTOP_SESSION" == 'hyprland-uwsm' ]]; then
+        uwsm stop
+    elif [[ "$DESKTOP_SESSION" == 'hyprland' ]]; then
+        command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit
+    else
+        notify-send -a "Rofi Power Menu" "Cannot logout!" \
+            "Undetermined session $DESKTOP_SESSION. Please implement it!"
+    fi
+}
+
 confirm_dialogue() {
 	echo -e "$yes\n$no" | rofi \
         -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
@@ -26,42 +37,23 @@ select_dialogue() {
 	echo -e "$shutdown\n$reboot\n$suspend\n$logout" | rofi -dmenu -i -theme ${theme}
 }
 
-session_logout() {
-    if [[ "$DESKTOP_SESSION" == 'hyprland-uwsm' ]]; then
-        uwsm stop
-    elif [[ "$DESKTOP_SESSION" == 'hyprland' ]]; then
-        command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit
-    else
-        notify-send -a "Rofi Power Menu" "Cannot logout!" \
-            "Undetermined session $DESKTOP_SESSION. Please implement it!"
-    fi
-}
-
 run_cmd() {
     [[ "$(confirm_dialogue)" == "$no" ]] && exit 0
 
-    if [[ $1 == '--shutdown' ]]; then
-        systemctl poweroff
-    elif [[ $1 == '--reboot' ]]; then
-        systemctl reboot
-    elif [[ $1 == '--suspend' ]]; then
-        systemctl suspend
-    elif [[ $1 == '--logout' ]]; then
-        session_logout
-    fi
+    case "$1" in
+        "$shutdown")
+            systemctl poweroff
+            ;;
+        "$reboot")
+            systemctl reboot
+            ;;
+        "$suspend")
+            systemctl suspend
+            ;;
+        "$logout")
+            session_logout
+            ;;
+    esac
 }
 
-case $(select_dialogue) in
-    $shutdown)
-		run_cmd --shutdown
-        ;;
-    $reboot)
-		run_cmd --reboot
-        ;;
-    $suspend)
-		run_cmd --suspend
-        ;;
-    $logout)
-		run_cmd --logout
-        ;;
-esac
+run_cmd "$(select_dialogue)"
