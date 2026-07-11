450ae0f320e4b52235648f29da807130622b7b86
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-07-11 21:08:11 +0700

    Add more key bindings for volume settings
    More fine-grained control over the volume settings

--- .config/hypr/modules/binds/audio.lua
+++ .config/hypr/modules/binds/audio.lua
@@ -1,28 +1,55 @@
 local main_mod = require("config")["main_mod"]
-local locked = { locked = true }
-local locked_and_repeating = { locked = true, repeating = true }
+local lck = { locked = true }
+local lck_and_rpt = { locked = true, repeating = true }
 local cmds = {
   mute = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle",
-  lower_vol = "wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 2%-",
-  inc_vol = "wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 2%+",
+  set_vol = "wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@",
   pause = "playerctl play-pause",
   play_next = "playerctl next",
   play_prev = "playerctl previous"
 }
 
 
-hl.bind(main_mod .. " + F1", hl.dsp.exec_cmd(cmds["mute"]), locked_and_repeating)
-hl.bind(main_mod .. " + F2", hl.dsp.exec_cmd(cmds["lower_vol"]), locked_and_repeating)
-hl.bind(main_mod .. " + F3", hl.dsp.exec_cmd(cmds["inc_vol"]), locked_and_repeating)
-hl.bind(main_mod .. " + F4", hl.dsp.exec_cmd(cmds["pause"]), locked)
+hl.bind(main_mod .. " + F1", hl.dsp.exec_cmd(cmds["mute"]), lck_and_rpt)
+hl.bind(main_mod .. " + F2", hl.dsp.exec_cmd(cmds["set_vol"] .. " 2%-"), lck_and_rpt)
+hl.bind(main_mod .. " + F3", hl.dsp.exec_cmd(cmds["set_vol"] .. " 2%+"), lck_and_rpt)
+hl.bind(main_mod .. " + F4", hl.dsp.exec_cmd(cmds["pause"]), lck)
 
 
-hl.bind("XF86AudioMute", hl.dsp.exec_cmd(cmds["mute"]), locked_and_repeating)
-hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd(cmds["lower_vol"]), locked_and_repeating)
-hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd(cmds["inc_vol"]), locked_and_repeating)
+hl.bind("XF86AudioMute", hl.dsp.exec_cmd(cmds["mute"]), lck_and_rpt)
+hl.bind(
+  "ALT + XF86AudioLowerVolume",
+  hl.dsp.exec_cmd(cmds["set_vol"] .. " 1%-"),
+  lck_and_rpt
+)
+hl.bind(
+  "ALT + XF86AudioRaiseVolume",
+  hl.dsp.exec_cmd(cmds["set_vol"] .. " 1%+"),
+  lck_and_rpt
+)
+hl.bind(
+  "XF86AudioLowerVolume",
+  hl.dsp.exec_cmd(cmds["set_vol"] .. " 2%-"),
+  lck_and_rpt
+)
+hl.bind(
+  "XF86AudioRaiseVolume",
+  hl.dsp.exec_cmd(cmds["set_vol"] .. " 2%+"),
+  lck_and_rpt
+)
+hl.bind(
+  "CTRL + XF86AudioLowerVolume",
+  hl.dsp.exec_cmd(cmds["set_vol"] .. " 5%-"),
+  lck_and_rpt
+)
+hl.bind(
+  "CTRL + XF86AudioRaiseVolume",
+  hl.dsp.exec_cmd(cmds["set_vol"] .. " 5%+"),
+  lck_and_rpt
+)
 
 
-hl.bind("XF86AudioPause", hl.dsp.exec_cmd(cmds["mute"]), locked)
-hl.bind("XF86AudioPlay", hl.dsp.exec_cmd(cmds["mute"]), locked)
-hl.bind("XF86AudioNext", hl.dsp.exec_cmd(cmds["play_next"]), locked)
-hl.bind("XF86AudioPrev", hl.dsp.exec_cmd(cmds["play_prev"]), locked)
+hl.bind("XF86AudioPause", hl.dsp.exec_cmd(cmds["mute"]), lck)
+hl.bind("XF86AudioPlay", hl.dsp.exec_cmd(cmds["mute"]), lck)
+hl.bind("XF86AudioNext", hl.dsp.exec_cmd(cmds["play_next"]), lck)
+hl.bind("XF86AudioPrev", hl.dsp.exec_cmd(cmds["play_prev"]), lck)
