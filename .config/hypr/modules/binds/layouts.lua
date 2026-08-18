299d48d7d16ad3683eef14350197f49ec4857be0
Trịnh Ngọc Các <cactrinh5@gmail.com>
2026-08-18 19:58:42 +0700

    Remove unused layout key-bindings

--- .config/hypr/modules/binds/layouts.lua
+++ .config/hypr/modules/binds/layouts.lua
@@ -2,10 +2,11 @@ local CONFIG = require("config")
 local main_mod = CONFIG["main_mod"]
 
 hl.bind(main_mod .. " + SHIFT + v", hl.dsp.window.float({ action = "toggle" }))
-hl.bind(main_mod .. " + P", hl.dsp.window.pseudo())
-hl.bind(main_mod .. " + J", hl.dsp.layout("togglesplit"))    -- dwindle only
+hl.bind(main_mod .. " + J", hl.dsp.layout("togglesplit"))
 
--- Move focus with main_mod + arrow keys
+-- Move focus
+hl.bind(main_mod .. "+ Tab", hl.dsp.layout("cyclenext"))
+hl.bind(main_mod .. "+ SHIFT + Tab", hl.dsp.layout("cycleprev"))
 hl.bind(main_mod .. "+ ALT + left",  hl.dsp.focus({ direction = "left" }))
 hl.bind(main_mod .. "+ ALT + right", hl.dsp.focus({ direction = "right" }))
 hl.bind(main_mod .. "+ ALT + up",    hl.dsp.focus({ direction = "up" }))
@@ -14,8 +15,6 @@ hl.bind(main_mod .. "+ ALT + h",     hl.dsp.focus({ direction = "left" }))
 hl.bind(main_mod .. "+ ALT + l",     hl.dsp.focus({ direction = "right" }))
 hl.bind(main_mod .. "+ ALT + k",     hl.dsp.focus({ direction = "up" }))
 hl.bind(main_mod .. "+ ALT + j",     hl.dsp.focus({ direction = "down" }))
-hl.bind(main_mod .. "+ Tab", hl.dsp.layout("cyclenext"))
-hl.bind(main_mod .. "+ SHIFT + Tab", hl.dsp.layout("cycleprev"))
 
 -- navigating workspaces
 hl.bind(main_mod .. " + SHIFT + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
